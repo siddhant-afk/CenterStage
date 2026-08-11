644bd5dd2f998feb31cf561f27a1a08ea8f49cdf
Siddhant Dudhane <siddhantdudhane21@gmail.com>
2026-08-11 16:39:53 +0530

    use default AWS credential provider chain

--- streaming-service/src/main/java/com/example/streamingservice/config/AppConfig.java
+++ streaming-service/src/main/java/com/example/streamingservice/config/AppConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -15,42 +13,30 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class AppConfig {
 
-    @Value("${aws.access-key}")
-    private String accessKey;
-
-    @Value("${aws.secret-key}")
-    private String secretKey;
-
     @Value("${aws.region}")
     private String region;
 
     @Bean
-    public S3Client s3Client(){
+    public S3Client s3Client() {
 
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey,secretKey)
-                ))
                 .build();
     }
 
     @Bean
-    public S3Presigner s3Presigner(){
+    public S3Presigner s3Presigner() {
 
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey,secretKey)
-                ))
                 .build();
     }
 
     @Bean
-    public RedisTemplate<String,String> redisTemplate(
+    public RedisTemplate<String, String> redisTemplate(
             RedisConnectionFactory redisConnectionFactory
-    ){
-        RedisTemplate<String,String> template = new RedisTemplate<>();
+    ) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
